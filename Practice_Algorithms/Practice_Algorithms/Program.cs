ff01926cd425eabbdf0cf36048a29a521463aec1
Macario Hernandez <mhernandez@nyccfb.info>
2021-01-12 21:08:56 -0500

    some algorithms

---
+++ Practice_Algorithms/Practice_Algorithms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Algorithms.Basic_Algorithms;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BasicAlgorithms.Gcd(66, 78));
+            Console.WriteLine(BasicAlgorithms.Lcm(12, 15));
+            Console.WriteLine(string.Join(" x ", BasicAlgorithms.PrimeFactorization(11)));
+            Console.WriteLine(string.Join(",", BasicAlgorithms.SieveOfEratosthenesAlgorithm(10000)));
+
+            Console.ReadKey();
+        }
+    }
+}
