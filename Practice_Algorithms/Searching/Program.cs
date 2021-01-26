143b743d35dba095588966e421a1168f060c19f6
Macario Hernandez <mackhdez@hotmail.com>
2021-01-26 01:49:28 -0500

    Search Algorithms (#3)
    Co-authored-by: Macario Hernandez <mackhdez@hotmail.com>

---
+++ Practice_Algorithms/Searching/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Searching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Linear Search
+            var linearData = new[] { 4, 6, 5, 3, 1, 2, 8, 9, 7 };
+            var linearIndex = Search<int>.Linear(linearData, 11);
+
+            Console.WriteLine($"{(linearIndex > 0 ? linearIndex : "Value not found")}");
+            #endregion
+
+            #region Binary Search
+            var binaryData = new[] { 0, 4, 7, 9, 12, 14, 18, 25, 27, 36, 46, 50, 64, 79, 88 };
+            var binaryIndex = Search<int>.Binary(binaryData, 77);
+
+            Console.WriteLine($"{(binaryIndex > 0 ? binaryIndex : "Value not found")}");
+            #endregion
+
+            #region Interpolation Search
+            var interpolationData = new long[] { 0, 7, 12, 17, 21, 26, 31, 33, 40, 43, 49, 51, 64, 66, 72, 73, 81, 87, 95, 99,100,110,111,115,117 };
+            var interpolationIndex = Search<int>.Interpolation(interpolationData, 110);
+
+            Console.WriteLine($"{(interpolationIndex > 0 ? interpolationIndex : "Value not found")}");
+            #endregion
+        }
+    }
+}
---
+++ Practice_Algorithms/Searching/Search.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Linq;
+
+namespace Searching
+{
+    public static class Search<T> where T : IComparable<T>
+    {
+        #region Linear Search
+        public static int Linear(T[] arr, T target)
+        {
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].CompareTo(target) == 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+        #endregion
+
+        #region Binary Search
+        public static int Binary(T[] arr, T target)
+        {
+            Array.Sort(arr);
+
+            var min = 0;
+            var max = arr.Length - 1;
+
+            while (min <= max)
+            {
+                var mid = (min + max) / 2;
+                var diff = target.CompareTo(arr[mid]);
+
+                switch (diff)
+                {
+                    case 0:
+                        return mid;
+                    case < 0:
+                        max = mid - 1;
+                        break;
+                    default:
+                        min = mid + 1;
+                        break;
+                }
+            }
+
+            return -1;
+        }
+        #endregion
+
+        #region Interpolation Search
+        public static int Interpolation(long[] arr, long target)
+        {
+            Array.Sort(arr);
+
+            //var distance = target - arr[0];
+            //var valueRange = arr[^1] - arr[0];
+            //double fraction = distance / valueRange;
+            //var indexRange = arr.Length - 1 - 0;
+            //var estimate = (int)(0 + fraction * indexRange);
+
+            var min = 0;
+            var max = arr.Length - 1;
+            var fraction = (target - arr[min]) / (double)(arr[max] - arr[0]);
+            var guess = (int)((max - min) * fraction);
+            if (guess < 0) return 0;
+            if (guess > max) guess = max;
+
+            if (target == arr[guess]) return guess;
+            if (target < arr[guess]) max = guess;
+            else min = guess;
+
+            // Binary search from this point.
+            while (min <= max)
+            {
+                var mid = (max + min) / 2;
+                var diff = target.CompareTo(arr[mid]);
+                switch (diff)
+                {
+                    case 0:
+                        return mid;
+                    case < 0:
+                        max = mid - 1;
+                        break;
+                    default:
+                        min = mid + 1;
+                        break;
+                }
+            }
+
+            return -1;
+        }
+        //public static int Interpolation(long[] arr, long target)
+        //{
+        //    Array.Sort(arr);
+
+        //    //var distance = target - arr[0];
+        //    //var valueRange = arr[^1] - arr[0];
+        //    //double fraction = distance / valueRange;
+        //    //var indexRange = arr.Length - 1 - 0;
+        //    //var estimate = (int)(0 + fraction * indexRange);
+
+        //    var min = 0;
+        //    var max = arr.Length - 1;
+        //    var fraction = (target - arr[min]) / (double)(arr[max] - arr[0]);
+        //    var guess = (int)((max - min) * fraction);
+        //    if (guess < 0) return 0;
+        //    if (guess > max) guess = max;
+
+        //    // Use an expanding binary search to bound the target.
+        //    if (target == arr[guess]) return guess;
+        //    if (target < arr[guess])
+        //    {
+        //        // Search down.
+        //        max = guess;
+        //        var offset = 1;
+        //        while (target < arr[guess])
+        //        {
+        //            guess -= offset;
+        //            if (guess < 0)
+        //            {
+        //                guess = 0;
+        //                break;
+        //            }
+        //            offset *= 2;
+        //        }
+        //        min = guess;
+        //    }
+        //    else
+        //    {
+        //        // Search up.
+        //        min = guess;
+        //        var offset = 1;
+        //        while (target > arr[guess])
+        //        {
+        //            guess += offset;
+        //            if (guess > max)
+        //            {
+        //                guess = max;
+        //                break;
+        //            }
+        //            offset *= 2;
+        //        }
+        //        max = guess;
+        //    }
+
+        //    // Binary search from this point.
+        //    while (min <= max)
+        //    {
+        //        var mid = (max + min) / 2;
+        //        var diff = target.CompareTo(arr[mid]);
+        //        switch (diff)
+        //        {
+        //            case 0:
+        //                return mid;
+        //            case < 0:
+        //                max = mid - 1;
+        //                break;
+        //            default:
+        //                min = mid + 1;
+        //                break;
+        //        }
+        //    }
+
+        //    return -1;
+        //}
+        #endregion
+    }
+}
